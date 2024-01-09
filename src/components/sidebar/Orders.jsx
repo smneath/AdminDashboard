ea0821c80cd92e43db86b304b673cdca2d127d7c
smneath <smneath77@gmail.com>
2024-01-09 16:26:10 +0700

    admin

---
+++ src/components/sidebar/Orders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Orders() {
+  return (
+    <div>Packages</div>
+  )
+}
+
+export default Orders
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import "./login.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      dispatch({ type: "LOGIN", payload: user });
+      console.log(auth)
+      console.log(email)
+
+      console.log(password)
+
+      navigate("/");
+    } catch (error) {
+      console.error("Authentication error:", error.code, error.message);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
